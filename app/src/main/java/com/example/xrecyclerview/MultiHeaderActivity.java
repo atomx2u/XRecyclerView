cf6c828b926231dedaef23a84dd27826061f4f6f
Atom Xu <27760063@qq.com>
2018-10-15 17:43:43 +0800

    migrate support libraries to androidX libraries

--- app/src/main/java/com/example/xrecyclerview/MultiHeaderActivity.java
+++ app/src/main/java/com/example/xrecyclerview/MultiHeaderActivity.java
@@ -1,11 +1,10 @@
 package com.example.xrecyclerview;
 
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.appcompat.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
